a1e2ba1a204af66160698d930ea73c45c2bf9565
Ģirts Alksnis <girtsa@inbox.lv>
2021-10-06 21:37:01 +0300

    Today_06_10

--- Exercises/Level1/Exercises.Level1/Array2.cs
+++ Exercises/Level1/Exercises.Level1/Array2.cs
@@ -17,19 +17,28 @@ namespace Exercises.Level1
         /// </summary>
         public int CountEvens(int[] nums)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                if (num % 2 == 0)
+                {
+                    sum++;
+                }
+            }
+            return sum;
         }
 
-        /// <summary>
-        /// Given an array length 1 or more of ints, return the difference between the largest and
-        /// smallest values in the array. Note: the built-in Math.min(v1, v2) and Math.max(v1, v2)
-        /// methods return the smaller or larger of two values.
-        /// 
-        /// bigDiff([10, 3, 5, 6]) → 7
-        /// bigDiff([7, 2, 10, 9]) → 8
-        /// bigDiff([2, 10, 7, 2]) → 8
-        /// </summary>
-        public int BigDiff(int[] nums)
+
+            /// <summary>
+            /// Given an array length 1 or more of ints, return the difference between the largest and
+            /// smallest values in the array. Note: the built-in Math.min(v1, v2) and Math.max(v1, v2)
+            /// methods return the smaller or larger of two values.
+            /// 
+            /// bigDiff([10, 3, 5, 6]) → 7
+            /// bigDiff([7, 2, 10, 9]) → 8
+            /// bigDiff([2, 10, 7, 2]) → 8
+            /// </summary>
+            public int BigDiff(int[] nums)
         {
             throw new NotImplementedException();
         }
